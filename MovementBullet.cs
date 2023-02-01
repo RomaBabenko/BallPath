7e72d667b4b21798fe06cfc3f805c6cf6677fbc7
Roman Babenko <babienko.roma99@gmail.com>
2023-02-01 18:47:13 +0200

    removed comments

--- MovementBullet.cs
+++ MovementBullet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 
 public class MovementBullet : MonoBehaviour
@@ -21,13 +21,6 @@ public class MovementBullet : MonoBehaviour
 
     private void MovementForward()
     {
-        //transform.position += new Vector3(0, 0, speedMov) * Time.deltaTime;
-        //transform.Rotate(new Vector3(speedRot, 0, 0));
-        //transform.Translate(Vector3.forward * speedMov * Time.deltaTime);
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, speedMov * Time.deltaTime);
-        //Vector3 movement = new Vector3(0, 0, transform.position.z * speedRot);
-        //rigidbody.AddForce(movement);
-
         rigidbody.AddForce(0, 0, _speedMov);
     }
 }
