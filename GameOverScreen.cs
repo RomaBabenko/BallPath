799e6ab226f7e02a3d0a8f2287bd378da01c049a
Roman Babenko <babienko.roma99@gmail.com>
2023-02-02 14:36:19 +0200

    rename scripts

---
+++ GameOverScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject _deathScreen;
+
+    public void GameOverScreen()
+    {
+        if (!_deathScreen.activeSelf)
+        {
+                Invoke("End", 1f);
+        }
+    }
+
+    private void End()
+    {
+        _deathScreen.SetActive(true);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
